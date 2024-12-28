804cf3a04aec7aa4d8d39a7d198656548c6b4f3f
Eugene Bachura <eugenebachura@gmail.com>
2024-12-28 01:28:44 +0100

    Change version

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_gamification';
-$plugin->version = 2024122700;
+$plugin->version = 2024122704;
 $plugin->requires = 2022041900.00; // Require Moodle 4.0.0.
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v1.0';
