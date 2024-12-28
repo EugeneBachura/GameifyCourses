681d1aaf8084ba80cbe9fa5ca609763c85f648b8
Eugene Bachura <eugenebachura@gmail.com>
2024-12-28 01:26:17 +0100

    Adding customization parameters

--- lib.php
+++ lib.php
@@ -11,3 +11,41 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Event observer for gamification plugin.
+ */
+class local_gamification_observer
+{
+
+    /**
+     * Award points when a user completes an activity.
+     *
+     * @param \core\event\course_module_completion_updated $event
+     */
+    public static function award_points($event)
+    {
+        global $DB;
+
+        $userid = $event->relateduserid;
+        $courseid = $event->courseid;
+
+        $record = $DB->get_record('local_gamification_points', [
+            'userid' => $userid,
+            'courseid' => $courseid,
+        ]);
+
+        if ($record) {
+            $record->points += 10;
+            $record->timemodified = time();
+            $DB->update_record('local_gamification_points', $record);
+        } else {
+            $DB->insert_record('local_gamification_points', [
+                'userid' => $userid,
+                'courseid' => $courseid,
+                'points' => 10,
+                'timemodified' => time(),
+            ]);
+        }
+    }
+}
--- settings.php
+++ settings.php
@@ -7,16 +7,34 @@
  * @copyright 2025 Eugene Bachura
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
-    $settings = new admin_settingpage('gamification', get_string('pluginname', 'gamification'));
+    $settings = new admin_settingpage('local_gamification', get_string('pluginname', 'local_gamification'));
 
     $settings->add(new admin_setting_configtext(
-        'local_gamification/points_per_task',
-        'Points per task',
-        'Number of points awarded per completed task.',
-        10
+        'local_gamification/points_task',
+        get_string('points_task', 'local_gamification'),
+        get_string('points_task_desc', 'local_gamification'),
+        10,
+        PARAM_INT
+    ));
+
+    $settings->add(new admin_setting_configtext(
+        'local_gamification/points_forum',
+        get_string('points_forum', 'local_gamification'),
+        get_string('points_forum_desc', 'local_gamification'),
+        5,
+        PARAM_INT
+    ));
+
+    $settings->add(new admin_setting_configtext(
+        'local_gamification/points_quiz',
+        get_string('points_quiz', 'local_gamification'),
+        get_string('points_quiz_desc', 'local_gamification'),
+        15,
+        PARAM_INT
     ));
 
     $ADMIN->add('localplugins', $settings);
